27c20b7c69d1522f49a3f3a2e149e36db5c65814
thatblindgeye <thatblindgeye@gmail.com>
2020-11-03 16:21:16 -0500

    Update javascript, css, and html
    Continued updates to Javascript functionality as well as minor css and html
    updates.

--- script.js
+++ script.js
@@ -1,57 +1,103 @@
 "use strict";
 
 const themeBtn = document.querySelector(".theme");
-const digits = document.querySelectorAll(".digit");
-const operators = document.querySelectorAll(".operator");
+const digitBtns = document.querySelectorAll(".digit");
+const operatorBtns = document.querySelectorAll(".operator");
 const percentBtn = document.querySelector(".percentage");
 const posNegBtn = document.querySelector(".pos-neg");
 const decimalBtn = document.querySelector(".decimal");
+const equalBtn = document.querySelector(".equal");
 const clearBtn = document.querySelector(".clear");
 const backspaceBtn = document.querySelector(".delete");
 let display = document.getElementById("main-display");
 let equation = document.querySelector(".equation");
+let operandA = "";
+let operandB = "";
+let operator = "";
 
 function toggleTheme() {
   const doc = document.body;
   doc.classList.toggle("light-theme");
 }
 
-function allClear() {
-  document.querySelector(".display").scrollTo(0, 0);
-  display.textContent = 0;
+function toggleNegative() {
+  display.value *= -1;
+}
+
+function clearAll() {
+  display.value = "0";
+  equation.value = "";
+  operandA = "";
+  operandB = "";
+  operator = "";
 }
 
 function backspace() {
-  if (display.textContent.length === 1) {
-    display.textContent = 0;
+  if (display.value.length === 1) {
+    display.value = 0;
   } else {
-    display.textContent = display.textContent.slice(0, (display.textContent.length - 1));
+    display.value = display.value.slice(0, (display.value.length - 1));
   }
 }
 
-function displayDigits(e) {
-  if (display.textContent.length < 16) {
-    display.textContent = `${display.textContent}` + `${e.target.textContent}`;
+function addDecimal() {
+  if (display.value.includes(".")) return;
+  display.value += ".";
+}
+
+function inputNumber(e) {
+  if (display.value.length < 16) {
+    if (e.button === 0) {
+      display.value += e.target.textContent;
+    } else {
+      display.value += e.key;
+    }
   }
-  if (display.textContent.startsWith("0") && display.textContent.length > 1) {
-    display.textContent = display.textContent.slice(1);
+  if (display.value.startsWith("0") && !display.value.startsWith("0.")) {
+    display.value = display.value.slice(1);
   }
 }
 
-digits.forEach((digits) => {
-  digits.addEventListener("click", displayDigits)
+function inputOperator(e) {
+  if (!operandB) {
+    operandA = Number(display.value);
+    if (e.button === 0) {
+      operator = e.target.textContent;
+      equation.value = operandA + " " + operator;
+    } else if (e.key) {
+      operator = e.key;
+      equation.value = operandA + " " + operator;
+    }
+  } else {
+    operate();
+  }
+  display.value = 0;
+}
+
+window.addEventListener("load", clearAll)
+
+digitBtns.forEach((digitBtns) => {
+  digitBtns.addEventListener("click", inputNumber)
+})
+window.addEventListener("keydown", (e) => {
+  if (isFinite(e.key)) {
+    inputNumber(e);
+  }
 })
 
-operators.forEach((operators) => {
-  operators.addEventListener("click", (e) => {
-    console.log(e.target.textContent);
-  })
+operatorBtns.forEach((operatorBtns) => {
+  operatorBtns.addEventListener("click", inputOperator)
+})
+window.addEventListener("keydown", (e) => {
+  if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/") {
+    inputOperator(e);
+  }
 })
 
-clearBtn.addEventListener("click", allClear)
+clearBtn.addEventListener("click", clearAll)
 window.addEventListener("keydown", (e) => {
   if (e.key === "c") {
-    allClear();
+    clearAll();
   }
 })
 
@@ -59,6 +105,21 @@ backspaceBtn.addEventListener("click", backspace)
 window.addEventListener("keydown", (e) => {
   if (e.key === "Backspace") {
     backspace();
+  }
+})
+
+posNegBtn.addEventListener("click", toggleNegative)
+window.addEventListener("keydown", (e) => {
+  if (e.key === "n") {
+    toggleNegative();
+  }
+})
+
+decimalBtn.addEventListener("click", addDecimal)
+themeBtn.addEventListener("click", toggleTheme)
+window.addEventListener("keydown", (e) => {
+  if (e.key === ".") {
+    addDecimal();
   }
 })
 
