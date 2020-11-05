168d75c41ef4290fea8616f8ce858d304e20ab39
thatblindgeye <thatblindgeye@gmail.com>
2020-11-04 22:13:03 -0500

    Add functions for Javascript
    Added operation functions and made updates to various functions in the
    Javascript file. Also made minor tweaks to the CSS/HTML files.

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
-This is my calculator project for The Odin Project.
+This is my calculator project for The Odin Project. By default the theme is set to the Dark Theme; the
+theme button (sun icon) will toggle between Dark and Light themes.
 
 Check out the live preview of my <a href="">Calculator</a>.
 
 To Do
 
-1. Javascript
+1. Javascript - IN PROGRESS OVERALL
    1a) Implement click/touch and keyboard support IN PROGRESS
-   1b) Have two areas in display: operation area (2 + 2) and main area (current input/total (4))
-   1c) Allow decimals to be used and round totals to nth decimal place PARTIALLY DONE
-   1d) Percentage button converts the entered number to a decimal
+   1b) Have displays: equation display (2 + 2) and main display (current input or result) DONE
+   1c) Allow decimals to be used and round totals to nth decimal place IN PROGRESS
+   1d) Percentage button converts the entered number to a decimal DONE
    1e) Backspace/delete button removes the most recent digit, Clear completely clears all data DONE
-   1f) Dividing by 0 returns "ERROR"
+   1f) Dividing by 0 returns "ERROR" DONE
 2. Create button to toggle light/dark theme DONE
 3. Tweak color schemes for themes/layout
--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 "use strict";
 
-const themeBtn = document.querySelector(".theme");
-const digitBtns = document.querySelectorAll(".digit");
-const operatorBtns = document.querySelectorAll(".operator");
-const percentBtn = document.querySelector(".percentage");
-const posNegBtn = document.querySelector(".pos-neg");
-const decimalBtn = document.querySelector(".decimal");
-const equalBtn = document.querySelector(".equal");
-const clearBtn = document.querySelector(".clear");
-const backspaceBtn = document.querySelector(".delete");
 let display = document.getElementById("main-display");
 let equation = document.querySelector(".equation");
 let operandA = "";
 let operandB = "";
 let operator = "";
+let result = "";
 
-function toggleTheme() {
-  const doc = document.body;
-  doc.classList.toggle("light-theme");
+function add(operator, num1, num2) {
+  return parseFloat(num1) + parseFloat(num2);
 }
 
-function toggleNegative() {
-  display.value *= -1;
+function subtract(operator, num1, num2) {
+  return parseFloat(num1) - parseFloat(num2);
+}
+
+function divide(operator, num1, num2) {
+  if (num2 === "0") {
+    return "ERROR";
+  } else {
+    return parseFloat(num1) / parseFloat(num2);
+  }
+}
+
+function multiply(operator, num1, num2) {
+  return parseFloat(num1) * parseFloat(num2);
 }
 
 function clearAll() {
@@ -30,9 +33,11 @@ function clearAll() {
   operandA = "";
   operandB = "";
   operator = "";
+  result = "";
 }
 
 function backspace() {
+  if (result) return;
   if (display.value.length === 1) {
     display.value = 0;
   } else {
@@ -40,92 +45,172 @@ function backspace() {
   }
 }
 
+function makePercent() {
+  if (result === "ERROR") return;
+  result = display.value / 100;
+  if (result.toString().length > 16) {
+    display.value = result.toPrecision(1);
+  } else {
+    display.value = result;
+  }
+}
+
+function toggleNegative() {
+  if (result === "ERROR") return;
+  display.value *= -1;
+}
+
 function addDecimal() {
+  if (result === "ERROR") return;
   if (display.value.includes(".")) return;
   display.value += ".";
 }
 
 function inputNumber(e) {
-  if (display.value.length < 16) {
-    if (e.button === 0) {
-      display.value += e.target.textContent;
-    } else {
-      display.value += e.key;
-    }
+  if (display.value.length === 16) return;
+  if (result) clearAll();
+  if (e.button === 0) {
+    display.value += e.target.textContent;
+  } else {
+    display.value += e.key.replace(" ","");
   }
   if (display.value.startsWith("0") && !display.value.startsWith("0.")) {
     display.value = display.value.slice(1);
-  }
+  } 
 }
 
 function inputOperator(e) {
-  if (!operandB) {
-    operandA = Number(display.value);
-    if (e.button === 0) {
+  if (result === "ERROR") return;
+  operandA = display.value;
+  if (e.button === 0) {
       operator = e.target.textContent;
       equation.value = operandA + " " + operator;
-    } else if (e.key) {
+      display.value = 0;
+  } else {
       operator = e.key;
       equation.value = operandA + " " + operator;
+      display.value = 0;
     }
-  } else {
-    operate();
-  }
-  display.value = 0;
 }
+
+function operate() {
+  if (result === "ERROR") return;
+  if (operandB === "") {
+    operandB = display.value;
+  } else {
+    operandA = display.value;
+  }
+  equation.value = operandA + " " + operator + " " + operandB + " =";
+  switch (operator) {
+    case "+":
+      result = add(operator, operandA, operandB);
+      break;
+    case "-":
+      result = subtract(operator, operandA, operandB);
+      break;
+    case "/":
+      result = divide(operator, operandA, operandB);
+      break;
+    case "*":
+      result = multiply(operator, operandA, operandB);
+      break;
+    default:
+      result = "ERROR";
+  }
+}
+
+function checkResult() {
+  if (isFinite(result) && result.toString().length > 16) {
+    display.value = result.toPrecision(1);
+  } else {
+      display.value = result;
+    }
+}
+
+function toggleTheme() {
+  document.body.classList.toggle("light-theme");
+}
+
 
 window.addEventListener("load", clearAll)
 
-digitBtns.forEach((digitBtns) => {
-  digitBtns.addEventListener("click", inputNumber)
+document.querySelectorAll(".digit").forEach(btn => {
+  btn.addEventListener("click", (e) => {
+    inputNumber(e);
+  })
 })
 window.addEventListener("keydown", (e) => {
-  if (isFinite(e.key)) {
+  if (isFinite(parseFloat(e.key))) {
     inputNumber(e);
+    console.log(typeof(e.key));
   }
 })
 
-operatorBtns.forEach((operatorBtns) => {
-  operatorBtns.addEventListener("click", inputOperator)
+document.querySelectorAll(".operator").forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    inputOperator(e);
+  })
 })
 window.addEventListener("keydown", (e) => {
   if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/") {
+    e.preventDefault("/"); // prevents Quick Find from launching in Firefox
     inputOperator(e);
   }
 })
 
-clearBtn.addEventListener("click", clearAll)
+document.querySelector(".equal").addEventListener("click", () => {
+  if (operandA === "") return;
+  operate();
+  checkResult();
+})
 window.addEventListener("keydown", (e) => {
-  if (e.key === "c") {
+  if (e.key === "Enter") {
+    e.preventDefault("Enter"); // prevents duplicate input if a button has focus from a mouse click
+    if (operandA === "") return;
+    operate();
+    checkResult();
+  }
+})
+
+document.querySelector(".clear").addEventListener("click", clearAll)
+window.addEventListener("keydown", (e) => {
+  if (e.key === "c" || e.key === "C") {
     clearAll();
   }
 })
 
-backspaceBtn.addEventListener("click", backspace)
+document.querySelector(".delete").addEventListener("click", backspace)
 window.addEventListener("keydown", (e) => {
   if (e.key === "Backspace") {
+    e.preventDefault("Backspace"); // prevents browser from navigating back one page
     backspace();
   }
 })
 
-posNegBtn.addEventListener("click", toggleNegative)
+document.querySelector(".pos-neg").addEventListener("click", toggleNegative)
 window.addEventListener("keydown", (e) => {
-  if (e.key === "n") {
+  if (e.key === "n" || e.key === "N") {
     toggleNegative();
   }
 })
 
-decimalBtn.addEventListener("click", addDecimal)
-themeBtn.addEventListener("click", toggleTheme)
+document.querySelector(".decimal").addEventListener("click", addDecimal)
 window.addEventListener("keydown", (e) => {
   if (e.key === ".") {
     addDecimal();
   }
 })
 
-themeBtn.addEventListener("click", toggleTheme)
+document.querySelector(".percentage").addEventListener("click", makePercent)
 window.addEventListener("keydown", (e) => {
-  if (e.key === "t") {
+  if (e.key === "%") {
+    makePercent();
+  }
+})
+
+document.querySelector(".theme").addEventListener("click", toggleTheme)
+window.addEventListener("keydown", (e) => {
+  if (e.key === "t" || e.key === "T") {
     toggleTheme();
   }
 })
